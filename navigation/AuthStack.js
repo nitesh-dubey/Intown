e5b45ca059754966d96103f85f3e58a5c697a8da
Nitesh Dubey <niteshdubey100@gmail.com>
2021-03-09 21:34:51 +0530

    First Commit

---
+++ navigation/AuthStack.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import SignupScreen from "../screens/SignupScreen";
+import LoginScreen from "../screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator initialRouteName={"Login"}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ header: () => null }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={SignupScreen}
+        options={({ navigation }) => ({
+          title: "",
+          headerStyle: {
+            backgroundColor: "#fff",
+            shadowColor: "#fff",
+            elevation: 0,
+          },
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
